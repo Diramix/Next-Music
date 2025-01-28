079a2c0babc1007e08142bda59a35472aad22f86
Diramix <danyagranny10@proton.me>
2025-01-28 23:29:36 +0300

    optimize info buttons code

--- src/app/info/info.js
+++ src/app/info/info.js
@@ -7,24 +7,18 @@ const title = `Next Music ${currentPkgVersion} By Diramix`
 document.querySelector(".nm_title").textContent = title;
 
 // Buttons
-document.getElementById("women").addEventListener("click", () => {
-    const nya = new Audio('./assets/nya.mp3');
-    nya.play();
-    shell.openExternal("https://shikimori.one/Diramix");
-});
+const buttonActions = {
+    women: () => {
+        const nya = new Audio('./assets/nya.mp3');
+        nya.play();
+        shell.openExternal("https://shikimori.one/Diramix");
+    },
+    discordBtn: () => shell.openExternal("https://discord.gg/ky6bcdy7KA"),
+    githubBtn: () => shell.openExternal("https://github.com/diramix"),
+    boostyBtn: () => shell.openExternal("https://boosty.to/diramix"),
+    youtubeBtn: () => shell.openExternal("https://www.youtube.com/@Diram1x")
+};
 
-document.getElementById("discordBtn").addEventListener("click", () => {
-    shell.openExternal("https://discord.gg/ky6bcdy7KA");
-});
-
-document.getElementById("githubBtn").addEventListener("click", () => {
-    shell.openExternal("https://github.com/diramix");
-});
-
-document.getElementById("boostyBtn").addEventListener("click", () => {
-    shell.openExternal("https://boosty.to/diramix");
-});
-
-document.getElementById("youtubeBtn").addEventListener("click", () => {
-    shell.openExternal("https://www.youtube.com/@Diram1x");
+Object.entries(buttonActions).forEach(([id, action]) => {
+    document.getElementById(id).addEventListener("click", action);
 });
